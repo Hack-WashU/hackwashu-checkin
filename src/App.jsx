ad3d73f0fc0662fefb88ffd2efe28d256ccaa5cb
Vinay Viswanathan <v.vinay@wustl.edu>
2023-08-29 21:31:24 -0500

    Updated styling, added error text, and added favicon

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ let sheetData;
 
 function App() {
   const [showTshirt, setShowTshirt] = useState(false);
+  const [error, setError] = useState("no-error");
   const [nameValue, setNameValue] = useState("");
   const [emailValue, setEmailValue] = useState("");
 
@@ -20,7 +21,9 @@ function App() {
     return (
       <div className="app">
         <div className="header">
-          Please show your screen to registration to receive your swag
+          <p className="headline">
+            Please show your screen to registration to receive your swag
+          </p>
         </div>
         <div className="t-shirt-box-parent">
           <div className="t-shirt-box">
@@ -32,7 +35,14 @@ function App() {
   }
   return (
     <div className="app">
-      <div className="header">HackWashU Check In 2023</div>
+      <div className="header">
+        <p className="headline">HackWashU Check In 2023</p>
+      </div>
+      <div className="header-error">
+        <p className={error}>
+          Please show your screen to registration to receive your swag
+        </p>
+      </div>
       <div className="check-in-box-parent">
         <div className="check-in-box">
           <input
@@ -52,12 +62,12 @@ function App() {
           <div
             className="button"
             onClick={() => {
-              submitButton(setShowTshirt, nameValue, emailValue).then(
-                (result) => {
-                  shirtsize = result;
+              submitButton(emailValue, setError).then((result) => {
+                shirtsize = result;
+                if (shirtsize !== "") {
                   setShowTshirt(true);
                 }
-              );
+              });
             }}
           >
             Submit
@@ -78,27 +88,38 @@ async function getSheetData() {
   return retvalue;
 }
 
-async function submitButton(setShowTshirt, nameValue, emailValue) {
+async function submitButton(emailValue, setError) {
+  let shirt = "";
+  setError("no-error");
   let person = sheetData.find((person) => {
+    if (person.Email.toLowerCase() == emailValue.toLowerCase()) {
+      shirt = person["T-shirt"];
+    }
     return person.Email.toLowerCase() == emailValue.toLowerCase();
   });
+  if (!person) {
+    setError("error");
+  }
   console.log(person);
-  await fetch("https://sheetdb.io/api/v1/6dsy2cvuzwdcr/Email/" + emailValue, {
-    method: "PATCH",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      data: {
-        CheckedIn: "Yes",
+  try {
+    await fetch("https://sheetdb.io/api/v1/6dsy2cvuzwdcr/Email/" + emailValue, {
+      method: "PATCH",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
-    }),
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
-  return person["T-shirt"];
+      body: JSON.stringify({
+        data: {
+          CheckedIn: "Yes",
+        },
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  } catch (error) {
+    console.log(error);
+  }
+  return shirt;
 }
 
 export default App;
